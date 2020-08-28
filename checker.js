1f3edc844d67eb2755d9d6ba38b0698faec279b9
Ram_Vaishnav <68468177+RV313@users.noreply.github.com>
2020-08-28 12:14:06 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -7,21 +7,9 @@ const { vital_bpm } = require('./bpm.js');
 const { vital_spo2 } = require('./spo2.js');
 const { vital_respRate } = require('./respRate.js');
 
-function vitalsAreOk(bpm,spo2,respRate)
-{
-    var b1;
-    var s1;
-    var r1;
-    b1 = vital_bpm(bpm);
-    s1 = vital_spo2(spo2);
-    r1 = vital_respRate(respRate);
-    if(b1 = s1 = r1 = true)
-       return true;
-    else 
-        return false;
-    //function vitalsAreOk(bpm, spo2, respRate) {
-    //  return (vital_bpm(bpm) && vital_spo2(spo2) && vital_respRate(respRate));
-    //}
+    function vitalsAreOk(bpm, spo2, respRate){
+     return (vital_bpm(bpm) && vital_spo2(spo2) && vital_respRate(respRate));
+    }
     
 }
 
