13f64c500e50acf1be65952ecfadf2e9459b101a
Ram_Vaishnav <68468177+RV313@users.noreply.github.com>
2020-08-28 12:12:07 +0530

    Update spo2.js

--- spo2.js
+++ spo2.js
@@ -1,14 +1,11 @@
-function vital_spo2(spo2)
-{
-    if(spo2<90)
-        {
-            //console.log("Patient has abnormal spo2");
-            //console.log("Attention required");
+function vital_spo2(spo2){
+    if(spo2<90){
+            console.log("Patient has abnormal spo2");
+            console.log("Attention required");
             return false;
         }
-    else
-    {
-        //console.log("Patient has normal spo2");
+    else{
+        console.log("Patient has normal spo2");
         return true;
     }
 }
