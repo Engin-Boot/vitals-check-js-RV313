23614a82a64b9c8f4816173b0a1548c37a8179f8
Ram_Vaishnav <68468177+RV313@users.noreply.github.com>
2020-08-28 12:12:49 +0530

    Update bpm.js

--- bpm.js
+++ bpm.js
@@ -1,11 +1,11 @@
 function vital_bpm(bpm){
     if(bpm<70 || bpm>150){
-            //console.log("Patient has abnormal bpm");
-            //console.log("Attention required");
+            console.log("Patient has abnormal bpm");
+            console.log("Attention required");
             return false;
         }
     else{
-        //console.log("Patient has normal bpm");
+        console.log("Patient has normal bpm");
         return true;
     }
 } 
