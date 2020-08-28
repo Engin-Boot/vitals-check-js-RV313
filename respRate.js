329508fb9d6fec314b596f4bca746331f1535256
RV313 <RV.13051995@gmail.com>
2020-08-28 11:48:06 +0530

    updated

--- respRate.js
+++ respRate.js
@@ -2,13 +2,13 @@ function vital_respRate(respRate)
 {
     if(respRate<30 || respRate>95)
         {
-            console.log("Patient has abnormal respRate");
-            console.log("Attention required");
+            //console.log("Patient has abnormal respRate");
+            //console.log("Attention required");
             return false;
         }
     else
     {
-        console.log("Patient has normal respRate");
+        //console.log("Patient has normal respRate");
         return true;
     }
 }
--- spo2.js
+++ spo2.js
@@ -2,13 +2,13 @@ function vital_spo2(spo2)
 {
     if(spo2<90)
         {
-            console.log("Patient has abnormal spo2");
-            console.log("Attention required");
+            //console.log("Patient has abnormal spo2");
+            //console.log("Attention required");
             return false;
         }
     else
     {
-        console.log("Patient has normal spo2");
+        //console.log("Patient has normal spo2");
         return true;
     }
 }
